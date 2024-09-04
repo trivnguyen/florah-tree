61d616e318b4fe6367e596e3b5d164e46159e036
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2024-09-04 15:52:52 -0400

    add new LR scheduler and time embedding

--- models/models_utils.py
+++ models/models_utils.py
@@ -1,7 +1,25 @@
 
+import numpy as np
 import torch
 import torch.nn as nn
 import math
+
+def get_activation(activation):
+    """ Get an activation function. """
+    if activation.name.lower() == 'identity':
+        return nn.Identity()
+    elif activation.name.lower() == 'relu':
+        return nn.ReLU(inplace=False)
+    elif activation.name.lower() == 'tanh':
+        return nn.Tanh()
+    elif activation.name.lower() == 'sigmoid':
+        return nn.Sigmoid()
+    elif activation.name.lower() == 'leaky_relu':
+        return nn.LeakyReLU(activation.leaky_relu_alpha, inplace=False)
+    elif activation.name.lower() == 'gelu':
+        return nn.GELU()
+    else:
+        raise ValueError(f'Unknown activation function: {activation.name}')
 
 class WarmUpCosineAnnealingLR(torch.optim.lr_scheduler.LambdaLR):
     def __init__(self, optimizer, decay_steps, warmup_steps, eta_min=0, last_epoch=-1):
@@ -18,42 +36,60 @@ class WarmUpCosineAnnealingLR(torch.optim.lr_scheduler.LambdaLR):
             0.5 * (1 + math.cos(math.pi * (step - self.warmup_steps) / (self.decay_steps - warmup_steps))))
 
 
-def get_activation(activation):
-    """ Get an activation function. """
-    if activation.name.lower() == 'identity':
-        return nn.Identity()
-    elif activation.name.lower() == 'relu':
-        return nn.ReLU()
-    elif activation.name.lower() == 'tanh':
-        return nn.Tanh()
-    elif activation.name.lower() == 'sigmoid':
-        return nn.Sigmoid()
-    elif activation.name.lower() == 'leaky_relu':
-        return nn.LeakyReLU(activation.leaky_relu_alpha)
-    elif activation.name.lower() == 'gelu':
-        return nn.GELU()
-    else:
-        raise ValueError(f'Unknown activation function: {activation.name}')
+class WarmUpCosineDecayLR(torch.optim.lr_scheduler.LambdaLR):
+    def __init__(
+        self, optimizer, init_value, peak_value, warmup_steps, decay_steps,
+        end_value=0.0, exponent=1.0, last_epoch=-1):
+        self.init_value = init_value
+        self.peak_value = peak_value
+        self.warmup_steps = warmup_steps
+        self.decay_steps = decay_steps
+        self.end_value = end_value
+        self.exponent = exponent
 
-def configure_optimizers(parameters, optimizer_args, scheduler_args=None):
-    """ Return optimizer and scheduler for Pytorch Lightning """
-    scheduler_args = scheduler_args or {}
+        super().__init__(
+            optimizer, self.lr_lambda, last_epoch=last_epoch)
+
+    def cosine_decay_schedule(self, step, init_value, decay_steps, alpha=0.0, exponent=1.0):
+        step = min(step, self.decay_steps)
+        cosine_decay = 0.5 * (1 + np.cos(np.pi * step / self.decay_steps))
+        decayed = (1 - alpha) * cosine_decay ** exponent + alpha
+        return init_value * decayed
+
+    def linear_schedule(self, step, init_value, peak_value, warmup_steps):
+        return init_value + (peak_value - init_value) * step / warmup_steps
+
+    def lr_lambda(self, step):
+        alpha = 0 if self.peak_value == 0 else self.end_value / self.peak_value
+        if step < self.warmup_steps:
+            return self.linear_schedule(
+                step, self.init_value, self.peak_value, self.warmup_steps)
+        return self.cosine_decay_schedule(
+            step - self.warmup_steps,
+            init_value=self.peak_value,
+            decay_steps=self.decay_steps - self.warmup_steps,
+            alpha=alpha,
+            exponent=self.exponent,
+        )
+
+def configure_optimizers(parameters, optimizer_args, scheduler_args):
+    """ Return optimizer and scheduler. """
 
     # setup the optimizer
     if optimizer_args.name == "Adam":
         return torch.optim.Adam(
-            parameters, lr=optimizer_args.lr,
+            parameters(), lr=optimizer_args.lr,
             weight_decay=optimizer_args.weight_decay)
     elif optimizer_args.name == "AdamW":
         return torch.optim.AdamW(
-            parameters, lr=optimizer_args.lr,
+            parameters(), lr=optimizer_args.lr,
             weight_decay=optimizer_args.weight_decay)
     else:
         raise NotImplementedError(
             "Optimizer {} not implemented".format(optimizer_args.name))
 
     # setup the scheduler
-    if scheduler_args.name is None:
+    if scheduler_args.get(name) is None:
         scheduler = None
     elif scheduler_args.name == 'ReduceLROnPlateau':
         scheduler =  torch.optim.lr_scheduler.ReduceLROnPlateau(
@@ -63,15 +99,23 @@ def configure_optimizers(parameters, optimizer_args, scheduler_args=None):
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
             optimizer, T_max=scheduler_args.T_max,
             eta_min=scheduler_args.eta_min)
+    elif scheduler_args.name == 'WarmUpCosineDecayLR':
+        scheduler = WarmUpCosineDecayLR(
+            optimizer,
+            init_value=scheduler_args.init_value,
+            peak_value=scheduler_args.peak_value,
+            warmup_steps=scheduler_args.warmup_steps,
+            decay_steps=scheduler_args.decay_steps,
+        )
     elif scheduler_args.name == 'WarmUpCosineAnnealingLR':
-        scheduler = models_utils.WarmUpCosineAnnealingLR(
+        scheduler = WarmUpCosineAnnealingLR(
             optimizer,
             decay_steps=scheduler_args.decay_steps,
             warmup_steps=scheduler_args.warmup_steps,
             eta_min=scheduler_args.eta_min)
     else:
         raise NotImplementedError(
-            "Scheduler {} not implemented".format(self.scheduler_args.name))
+            "Scheduler {} not implemented".format(scheduler_args.name))
 
     if scheduler is None:
         return optimizer
@@ -85,4 +129,24 @@ def configure_optimizers(parameters, optimizer_args, scheduler_args=None):
                 'frequency': 1
             }
         }
+
+
+
+def get_timestep_embedding(
+    timesteps, embedding_dim, dtype=torch.float32, device='cuda'):
+    """Build sinusoidal embeddings (from Fairseq)."""
+
+    assert len(timesteps.shape) == 1
+    timesteps = timesteps * 1000
+
+    half_dim = embedding_dim // 2
+    emb = torch.log(torch.tensor(10_000, dtype=dtype, device=device)) / (half_dim - 1)
+    emb = torch.exp(torch.arange(half_dim, dtype=dtype, device=device) * -emb)
+    emb = timesteps.type(dtype)[:, None] * emb[None, :]
+    emb = torch.concatenate([torch.sin(emb), torch.cos(emb)], axis=1)
+    if embedding_dim % 2 == 1:  # Zero pad
+        emb = F.pad(emb, (0, 1))
+        # emb = jax.lax.pad(emb, dtype(0), ((0, 0, 0), (0, 1, 0)))
+    assert emb.shape == (timesteps.shape[0], embedding_dim)
+    return emb
 
