e841e9e5e7f248be4a317ebc7f3c1d5da0dc4eaf
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2024-09-06 12:16:57 -0400

    add context to decoder

--- models/transformer.py
+++ models/transformer.py
@@ -1,4 +1,6 @@
 
+from typing import Optional
+
 import torch
 import torch.nn as nn
 import math
@@ -7,7 +9,7 @@ from models.models import MLP
 from models import models_utils
 
 class FourierTimeEmbedding(nn.Module):
-    def __init__(self, emb_size: int):
+    def __init__(self, emb_size: int) -> None:
         super().__init__()
         if emb_size % 2 != 0:
             raise ValueError("Embedding size must be even")
@@ -15,7 +17,7 @@ class FourierTimeEmbedding(nn.Module):
         # randomly initialize frequencies for the Fourier transformation
         self.B = nn.Parameter(torch.randn(emb_size // 2, 1) * 2 * math.pi)
 
-    def forward(self, time):
+    def forward(self, time) -> torch.Tensor:
         # Apply sine and cosine functions for Fourier transformation
         time_proj = torch.matmul(time.unsqueeze(-1), self.B.T)
         time_emb = torch.cat([torch.sin(time_proj), torch.cos(time_proj)], dim=-1)
@@ -26,7 +28,7 @@ class FourierTimeEmbedding(nn.Module):
 
 class PositionalEmbedding(nn.Module):
     """ Positional embedding, adopted from PyTorch tutorial """
-    def __init__(self, emb_size: int, maxlen: int = 5000):
+    def __init__(self, emb_size: int, maxlen: int = 5000) -> None:
         super().__init__()
         if emb_size % 2 != 0:
             raise ValueError("Embedding size must be even")
@@ -39,20 +41,21 @@ class PositionalEmbedding(nn.Module):
         pos_embedding = pos_embedding.unsqueeze(0)
         self.register_buffer('pos_embedding', pos_embedding)
 
-    def forward(self, seq_len):
+    def forward(self, seq_len) -> torch.Tensor:
         return self.pos_embedding[:, :seq_len]
 
 class TransformerEncoder(nn.Module):
+    """ Transformer encoder with Fourier positional encoding """
     def __init__(
         self,
-        d_in,
-        d_model,
-        nhead,
-        dim_feedforward,
-        num_layers,
-        emb_size=16,
-        emb_dropout=0.1,
-    ):
+        d_in: int,
+        d_model: int,
+        nhead: int = 1,
+        dim_feedforward: int = 128,
+        num_layers: int = 1,
+        emb_size: int = 16,
+        emb_dropout: float = 0.1,
+    ) -> None:
         super().__init__()
         self.d_in = d_in
         self.d_model = d_model
@@ -63,19 +66,26 @@ class TransformerEncoder(nn.Module):
         self.emb_dropout = emb_dropout
         self._setup_model()
 
-    def _setup_model(self):
+    def _setup_model(self) -> None:
+        # embedding layers
         self.embedding = nn.Linear(self.d_in, self.emb_size)
         self.time_embedding = FourierTimeEmbedding(self.emb_size)
         self.dropout = nn.Dropout(self.emb_dropout)
         self.mlp = MLP(self.emb_size, [self.d_model, self.d_model * 4, self.d_model])
 
+        # transformer encoder
         self.transformer_encoder = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(
                 d_model=self.d_model, nhead=self.nhead,
                 dim_feedforward=self.dim_feedforward, batch_first=True),
             self.num_layers)
 
-    def forward(self, src, src_t, padding_mask=None):
+    def forward(
+        self,
+        src: torch.Tensor,
+        src_t: torch.Tensor,
+        src_padding_mask: Optional[torch.Tensor] = None
+    ) -> torch.Tensor:
 
         # embed the input and timestep
         x = self.embedding(src) + self.time_embedding(src_t)
@@ -83,25 +93,29 @@ class TransformerEncoder(nn.Module):
         x = self.mlp(x)
 
         # pass through the transformer
-        output = self.transformer_encoder(x, src_key_padding_mask=padding_mask)
+        output = self.transformer_encoder(
+            x, src_key_padding_mask=src_padding_mask)
 
         return output
 
 class TransformerDecoder(nn.Module):
+    """ Transformer encoder with positional encoding and context """
     def __init__(
         self,
-        d_in,
-        d_model,
-        nhead,
-        dim_feedforward,
-        num_layers,
-        emb_size=16,
-        emb_dropout=0.1,
-        max_len=3,
-    ):
+        d_in: int,
+        d_model: int,
+        d_context: Optional[int] = None,
+        nhead: int = 1,
+        dim_feedforward: int = 128,
+        num_layers: int = 1,
+        emb_size: int = 16,
+        emb_dropout: float = 0.1,
+        max_len: int = 3,
+    ) -> None:
         super().__init__()
         self.d_in = d_in
         self.d_model = d_model
+        self.d_context = d_context
         self.nhead = nhead
         self.dim_feedforward = dim_feedforward
         self.num_layers = num_layers
@@ -110,23 +124,44 @@ class TransformerDecoder(nn.Module):
         self.max_len = max_len
         self._setup_model()
 
-    def _setup_model(self):
+    def _setup_model(self) -> None:
+        # embedding layers
         self.embedding = nn.Linear(self.d_in, self.emb_size)
         self.postional_embedding = PositionalEmbedding(self.emb_size, self.max_len)
         self.dropout = nn.Dropout(self.emb_dropout)
         self.mlp = MLP(self.emb_size, [self.d_model, self.d_model * 4, self.d_model])
 
+        # context layers
+        if self.d_context is not None:
+            self.context_mlp = MLP(
+                self.d_context, [self.d_model, self.d_model * 4, self.d_model])
+        else:
+            self.context_mlp = None
+
+        # transformer decoder
         self.transformer_decoder = nn.TransformerDecoder(
             nn.TransformerDecoderLayer(
                 d_model=self.d_model, nhead=self.nhead,
                 dim_feedforward=self.dim_feedforward, batch_first=True),
             self.num_layers)
 
-    def forward(self, tgt, cond, tgt_padding_mask=None, cond_padding_mask=None):
+    def forward(
+        self,
+        tgt: torch.Tensor,
+        memory: torch.Tensor,
+        context: Optional[torch.Tensor] = None,
+        tgt_padding_mask: Optional[torch.Tensor] = None,
+        memory_padding_mask: Optional[torch.Tensor] = None
+    ) -> torch.Tensor:
 
         x = self.embedding(tgt) + self.postional_embedding(tgt.size(1))
         x = self.dropout(x)
         x = self.mlp(x)
+
+        if self.context_mlp is not None:
+            context = self.context_mlp(context)
+            context = context.unsqueeze(1).expand(-1, x.size(1), -1)
+            x = x + context
 
         # create casual mask
         tgt_mask = models_utils.get_casual_mask(x.size(1), device=x.device)
@@ -134,10 +169,10 @@ class TransformerDecoder(nn.Module):
         # pass through the transformer_decoder
         output = self.transformer_decoder(
             x,
-            cond,
+            memory,
             tgt_mask=tgt_mask,
             tgt_key_padding_mask=tgt_padding_mask,
-            memory_key_padding_mask=cond_padding_mask,
+            memory_key_padding_mask=memory_padding_mask,
         )
 
         return output
