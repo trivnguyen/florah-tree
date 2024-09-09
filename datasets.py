4ec9ebdfa1c5fffbef8996c68dd5252d4925f9a5
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2024-09-09 09:58:20 -0400

    min-max scaling time instead

--- datasets.py
+++ datasets.py
@@ -43,18 +43,35 @@ def prepare_dataloader(
 
     # calculate the normaliziation statistics
     if norm_dict is None:
-        x = torch.cat([d.x for d in data[:num_train]])
+        x = torch.cat([d.x[..., :-1] for d in data[:num_train]])
+        t = torch.cat([d.x[..., -1:] for d in data[:num_train]])
+
+        # standardize the input features and min-max normalize the time
         x_loc = x.mean(dim=0)
         x_scale = x.std(dim=0)
+        t_loc = t.min()
+        t_scale = t.max() - t_loc
+
         norm_dict = {
             "x_loc": list(x_loc.numpy()),
             "x_scale": list(x_scale.numpy()),
+            "t_loc": t_loc.numpy(),
+            "t_scale": t_scale.numpy(),
         }
     else:
         x_loc = torch.tensor(norm_dict["x_loc"], dtype=torch.float32)
         x_scale = torch.tensor(norm_dict["x_scale"], dtype=torch.float32)
+        t_loc = torch.tensor(norm_dict["t_loc"], dtype=torch.float32)
+        t_scale = torch.tensor(norm_dict["t_scale"], dtype=torch.float32)
     for d in data:
-        d.x = (d.x - x_loc) / x_scale
+        d.x[..., :-1] = (d.x[..., :-1] - x_loc) / x_scale
+        d.x[..., -1:] = (d.x[..., -1:] - t_loc) / t_scale
+
+    print("Normalization statistics:")
+    print("x_loc: {}".format(x_loc))
+    print("x_scale: {}".format(x_scale))
+    print("t_loc: {}".format(t_loc))
+    print("t_scale: {}".format(t_scale))
 
     # create data loader
     train_loader = DataLoader(
